d5bfcadb482dd143f2cd52824ffbff0cc8671742
Jehwan Yun <cluxebot@gmail.com>
2018-04-17 20:02:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(name='kiwi-steg',
+      version='0.4',
+      description='Image Steganography Sandbox',
+      url='https://github.com/Team-Lapio/kiwi-steg',
+      author='Team Lapio',
+      author_email='teamlapio@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+          'pyqt5',
+          'pillow'
+      ],
+      python_requires='>=3',
+      zip_safe=False)
